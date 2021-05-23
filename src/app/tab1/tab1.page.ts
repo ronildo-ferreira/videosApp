01fb52d40e3df39b2552c1cbdc33e2b126e9dc47
Ronildo Ap Ferreira <ronildo2020epni@gmail.com>
2021-05-23 11:20:28 -0300

    Adicionamos o componete Alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(public alertController: AlertController) {}
 
-  constructor() {}
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta!',
+      message: 'Deseja realmente favoritar o filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          },
+        },
+      ],
+    });
 
+    await alert.present();
+  }
 }
