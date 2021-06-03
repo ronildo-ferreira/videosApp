9ddc205684cffd5c60ceb1fea479db1036c4d011
Ronildo Ap Ferreira <ronildo2020epni@gmail.com>
2021-06-03 11:17:27 -0300

    Criamos a página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'dados-filme',
     loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  email: string;
+  senha: string;
+  constructor(public toastController: ToastController, private route: Router) {}
+
+  ngOnInit() {}
+  login() {
+    if (this.email === 'admin@admin.com' && this.senha === 'admin') {
+      this.route.navigateByUrl('tabs/tab1');
+      this.presentToast('Seja Bem Vindo!', 'success');
+    } else {
+      this.presentToast('Erro! E-mail e/ou Senha inválida!', 'danger');
+    }
+  }
+  async presentToast(texto: string, cor: string) {
+    const toast = await this.toastController.create({
+      message: texto,
+      duration: 2000,
+      color: cor,
+    });
+    toast.present();
+  }
+}
